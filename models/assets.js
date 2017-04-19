4cb79fc6a878f88b595dd7d65e631954f373e563
Sujanth <sujanth1987@gmail.com>
2017-04-19 16:57:00 +1000

    created table for assets show page

--- models/assets.js
+++ models/assets.js
@@ -4,7 +4,7 @@ var AssetsSchema = mongoose.Schema({
     name: String,
     asset_id: String,
     status: String,
-    user: String
+    description: String
 });
 
 module.exports = mongoose.model("Assets", AssetsSchema);
